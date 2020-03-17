76440a5f38b1d91dac90398187ef3e840d8d6be4
Alexey_Yudin <aviordream@gmail.com>
2020-03-18 00:23:02 +0300

    feat: add min, max and average function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || !array.length) {
+        return 0
+    }
+    let min = +Infinity;
+    array.forEach(item => {
+        if (item < min) {
+            min = item;
+        }
+    })
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0
+    }
+    let max = -Infinity;
+    array.forEach(item => {
+        if (item > max) {
+            max = item;
+        }
+    })
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0
+    }
+    let avg = 0;
+    array.forEach(item => {
+        avg += item;
+    })
+    avg = avg / array.length;
+    return avg;
 }
